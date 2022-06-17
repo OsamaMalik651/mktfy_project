e5093c3d6e8f6ea60c363a104f721bf17b00e573
OsamaMalik651 <osamamalik124@outlook.com>
2022-06-16 21:36:19 -0600

    Added TOS/Privacy component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Pages/LoginPage/Login';
 import SuccessAnimation from "./components/SuccessAnimation/SuccessAnimation";
+import TOS from "./components/TosAndPrivacyPolicy/TOS";
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/*" exact element={<Login />} />
         <Route path="/success" element={<SuccessAnimation />} />
-
+        <Route path="/terms-and-services" element={<TOS content="TOS" />} />
+        <Route path="/privacy-policy" element={<TOS content="PP" />} />
       </Routes>
     </Router>
   );
